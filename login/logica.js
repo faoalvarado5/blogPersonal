b592e92295de3d2f20bc07334057d25762b0b6d6
faoalvarado5 <44043200+faoalvarado5@users.noreply.github.com>
2019-12-15 22:15:38 -0600

    Creacion de la estructura del log in
    Creacion del esqueleto

---
+++ login/logica.js
@@ -0,0 +1,19 @@
+function myFunction1() {
+
+	window.alert(Cuenta registrada correctamente.);
+}
+
+function myFunction2() {
+
+	window.alert(Contrasena cambiada correctamente.);
+}
+
+function myFunction3() {
+
+	window.location("./registrarse.html");
+}
+
+function myFunction4() {
+
+	window.location("./olvidar.html");
+}
