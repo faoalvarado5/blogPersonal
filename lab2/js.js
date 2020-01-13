974c322262e3b17163048bff3ee143d544aca18e
faoalvarado5 <44043200+faoalvarado5@users.noreply.github.com>
2020-01-13 16:13:52 -0600

    Cuadro de direcciones
    Se anadio el cuadro de direcciones.

--- lab2/js.js
+++ lab2/js.js
@@ -1,36 +1,29 @@
 window.onload = function() {
 
-	var locations = new Array();
-	const xhttp = new XMLHttpRequest();
+	var waypoints = new Array();
+ 	const xhttp = new XMLHttpRequest();
 	xhttp.open('GET', 'direcciones.json', true);
 	xhttp.send();
 	xhttp.onreadystatechange = function(){
 
 		if(this.readyState == 4 && this.status == 200){
-
+			
 			let datos = JSON.parse(this.responseText);
 
 			for(let item of datos){
-				var temp = [{ latLng: { lat:   item.latitud  , lng:   item.longitud  } }];
-				locations = locations.concat(temp);
+				var tmp = L.latLng(item.latitud, item.longitud);
+				waypoints = waypoints.concat(tmp);
 			}
 
-			var map, dir;
+			var map = L.map('map').
+			setView([10.07192, -84.31205], 
+			14);
+			 
+			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(map);
 
-			map = L.map('map', {
-				layers: MQ.mapLayer(),
-				center: [ 10.02929,-84.29120 ],
-				zoom: 15
-			});
-
-			dir = MQ.routing.directions();
-			
-			dir.route({locations}); 
-			
-			map.addLayer(MQ.routing.routeLayer({
-				directions: dir,
-				fitBounds: true
-			}));
+			L.Routing.control({waypoints}).addTo(map);
 			
 			map.locate({setView: true, watch: true}).on('locationfound', function(e){
 			
